d4bb71bb54c5bfafa56c1d46785bb8cbdd284cf8
ehill111 <ehill111@comcast.net>
2020-08-11 16:23:23 -0400

    Created constructor for Dinosaur.cs.

--- RobotsVsDinosaursGame/Dinosaur.cs
+++ RobotsVsDinosaursGame/Dinosaur.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace RobotsVsDinosaursGame
@@ -16,5 +17,30 @@ namespace RobotsVsDinosaursGame
         string dinosaur2;
         string dinosaur3;
 
-    }
+        public Dinosaur(string name, string health, int powerLevel, string weapon, string attackPower, string energy)
+        {
+            this.name = name;
+            this.health = health;
+            this.powerLevel = powerLevel;
+            this.weapon = weapon;
+            this.attackPower = attackPower;
+            this.energy = energy;
+        }
+
+
+        
+        
+
+
+            
+     }   
+        
+       
+            
+
 }
+
+
+
+
+
